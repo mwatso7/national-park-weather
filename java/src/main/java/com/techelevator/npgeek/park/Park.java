df47924688c373ca33d6e9e7df72ca651c51836c
Andrew Frank <frank.229@gmail.com>
2019-07-09 16:04:16 -0400

    park pojo done

---
+++ java/src/main/java/com/techelevator/npgeek/park/Park.java
@@ -0,0 +1,167 @@
+package com.techelevator.npgeek.park;
+
+public class Park {
+	
+	//Data Members************************************************************************************************
+	
+	private String parkcode;
+	private String parkname;
+	private String state;
+	private int acreage;
+	private int elevationinfeet;
+	private double milesoftrail;
+	private int numberofcampsites;
+	private String climate;
+	private int yearfounded;
+	private int annualvisitorcount;
+	private String inspirationalquote;
+	private String inspirationalquotesource;
+	private String parkdescription;
+	private int entryfee;
+	private int numberofanimalspecies;
+	
+	//CTOR********************************************************************************************************
+	
+	public Park() {
+		String parkcode = "unassigned";
+		String parkname = "unassigned";
+		String state = "unassigned";
+		int acreage = 0;
+		int elevationinfeet = 0;
+		double milesoftrail = 0.0;
+		int numberofcampsites = 0;
+		String climate = "unassigned";
+		int yearfounded = 0;
+		int annualvisitorcount = 0;
+		String inspirationalquote = "unassigned";
+		String inspirationalquotesource = "unassigned";
+		String parkdescription = "unassigned";
+		int entryfee = 0;
+		int numberofanimalspecies = 0;
+	}
+		
+	//Get/Set*****************************************************************************************************
+	
+	public String getParkcode() {
+		return parkcode;
+	}
+
+	public void setParkcode(String parkcode) {
+		this.parkcode = parkcode;
+	}
+
+	public String getParkname() {
+		return parkname;
+	}
+
+	public void setParkname(String parkname) {
+		this.parkname = parkname;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getAcreage() {
+		return acreage;
+	}
+
+	public void setAcreage(int acreage) {
+		this.acreage = acreage;
+	}
+
+	public int getElevationinfeet() {
+		return elevationinfeet;
+	}
+
+	public void setElevationinfeet(int elevationinfeet) {
+		this.elevationinfeet = elevationinfeet;
+	}
+
+	public double getMilesoftrail() {
+		return milesoftrail;
+	}
+
+	public void setMilesoftrail(double milesoftrail) {
+		this.milesoftrail = milesoftrail;
+	}
+
+	public int getNumberofcampsites() {
+		return numberofcampsites;
+	}
+
+	public void setNumberofcampsites(int numberofcampsites) {
+		this.numberofcampsites = numberofcampsites;
+	}
+
+	public String getClimate() {
+		return climate;
+	}
+
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+
+	public int getYearfounded() {
+		return yearfounded;
+	}
+
+	public void setYearfounded(int yearfounded) {
+		this.yearfounded = yearfounded;
+	}
+
+	public int getAnnualvisitorcount() {
+		return annualvisitorcount;
+	}
+
+	public void setAnnualvisitorcount(int annualvisitorcount) {
+		this.annualvisitorcount = annualvisitorcount;
+	}
+
+	public String getInspirationalquote() {
+		return inspirationalquote;
+	}
+
+	public void setInspirationalquote(String inspirationalquote) {
+		this.inspirationalquote = inspirationalquote;
+	}
+
+	public String getInspirationalquotesource() {
+		return inspirationalquotesource;
+	}
+
+	public void setInspirationalquotesource(String inspirationalquotesource) {
+		this.inspirationalquotesource = inspirationalquotesource;
+	}
+
+	public String getParkdescription() {
+		return parkdescription;
+	}
+
+	public void setParkdescription(String parkdescription) {
+		this.parkdescription = parkdescription;
+	}
+
+	public int getEntryfee() {
+		return entryfee;
+	}
+
+	public void setEntryfee(int entryfee) {
+		this.entryfee = entryfee;
+	}
+
+	public int getNumberofanimalspecies() {
+		return numberofanimalspecies;
+	}
+
+	public void setNumberofanimalspecies(int numberofanimalspecies) {
+		this.numberofanimalspecies = numberofanimalspecies;
+	}
+	
+	//Methods*****************************************************************************************************
+
+}
